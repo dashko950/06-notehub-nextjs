6df8b2dcfaa5996d8df9275fedb03fdb22cc3d9e
VASYL SPASHCHUK <buyapple0205@gmail.com>
2026-04-10 12:47:37 +0300

    chore: fix types and add placeholder components for homework

--- types/note.ts
+++ types/note.ts
@@ -1,12 +1,10 @@
-// Пусті або базові типи для проєкту
-
-
+// Базові типи для проєкту
 
 export interface Note {
-      id: string,
-      title: string,
-      content: string,
-      createdAt: string,
-      updatedAt: string,
-      tag: "Todo" | "Work" | "Personal"| "Meeting"| "Shopping"
-    }
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  tag: 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+}
